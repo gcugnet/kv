a9f76a382360eb9cb24e5af4169f6057108cd641
Guillaume Cugnet <guillaume@cugnet.eu>
2024-06-14 12:26:07 +0200

    create 'Bucket' Agent

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,23 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes a key and returns it values", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 6)
+
+    deleted_value = KV.Bucket.delete(bucket, "eggs")
+    assert deleted_value == 6
+    assert KV.Bucket.get(bucket, "eggs") == nil
+  end
+end
