7faaddd38ea7fcaae47aac4c34c1494f7e304880
Guillaume Cugnet <guillaume@cugnet.eu>
2024-06-14 16:24:55 +0200

    create a client API to enhance access on the 'Registry' GenServer

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -5,7 +5,28 @@ defmodule KV.Registry do
   #                            Client API (process)                            #
   # -------------------------------------------------------------------------- #
 
-  # Will be added later.
+  @doc """
+  Starts the registry.
+  """
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  @doc """
+  Looks up the bucket pid for 'name' stored in 'server'.
+
+  Returns '{:ok, pid}' if the bucket exists, 'error' otherwise.
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Ensures there is a bucket associated with the given 'name' in 'server'.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
 
   # -------------------------------------------------------------------------- #
   #                   GeneServer Callbacks: Server (process)                   #
